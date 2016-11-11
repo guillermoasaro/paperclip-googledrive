aaf44519325b2e3f406eb8adea5208177c8ecd03
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-11-10 19:36:39 -0500

    add spec files and basic structure

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'alegra'
+Dir[File.join('./lib/paperclip/**/*.rb')].each { |f| require f }
+Dir[File.join('./spec/support/**/*.rb')].each { |f| require f }
+require 'rspec'
+require 'pry'
+
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/cassettes"
+  c.hook_into :webmock
+end
+
+# RSpec.configure do |config|
+#   config.include SomeModuleHere
+# end
