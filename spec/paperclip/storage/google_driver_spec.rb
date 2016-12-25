b2d8717a4fe44f098d14ca62f68db9e92551a684
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-12-25 13:53:06 -0500

    add spec for updload pdf files

--- spec/paperclip/storage/google_driver_spec.rb
+++ spec/paperclip/storage/google_driver_spec.rb
@@ -1,41 +1,42 @@
 require 'spec_helper'
 
 describe 'Paperclip::Storage::GoogleDrive' do
+  let(:dummy) do
+    rebuild_model(
+      storage: :google_drive,
+      google_drive_client_secret_path: 'spec/support/client_secret.json',
+      styles: { medium: '300x300' },
+      google_drive_options: {
+        application_name: 'test-app',
+        public_folder_id: '0B-GFJI5FWVGyQXFKRzkydldoalk',
+        path: proc { |style| "#{style}_#{id}_#{document.original_filename}" }
+      }
+    )
+    Dummy.new
+  end
+
   context 'Manage an image' do
-    before do
-      rebuild_model(
-        storage: :google_drive,
-        google_drive_client_secret_path: 'spec/support/client_secret.json',
-        styles: { medium: '300x300' },
-        google_drive_options: {
-          application_name: 'test-app',
-          public_folder_id: '0B-GFJI5FWVGyQXFKRzkydldoalk',
-          path: proc { |style| "#{style}_#{id}_#{document.original_filename}" }
-        }
-      )
-      @dummy = Dummy.new
-    end
 
     it 'should upload an image' do
       VCR.use_cassette('upload_image') do
-        @dummy.document = File.new('spec/fixtures/image.png', 'rb')
-        expect(@dummy.document).to_not be_blank
-        expect(@dummy.document).to be_present
-        expect(@dummy.save).to be true
-        expect(@dummy.document.url).to be_present
+        dummy.document = File.new('spec/fixtures/image.png', 'rb')
+        expect(dummy.document).to_not be_blank
+        expect(dummy.document).to be_present
+        expect(dummy.save).to be true
+        expect(dummy.document.url).to be_present
       end
     end
 
     it 'should destroy an image' do
       VCR.use_cassette('remove_image') do
-        @dummy.save
-        @dummy.update_column(:document_file_name, 'image.png')
-        @dummy.update_column(:document_content_type, 'image/png')
-        @dummy.update_column(:document_fingerprint, 'c5591c5ae4d01cae00d27b1cfb95fb2e')
-        @dummy.destroy
-        expect(@dummy.document_file_name).to eq nil
-        expect(@dummy.document_content_type).to eq nil
-        expect(@dummy.document_fingerprint).to eq nil
+        dummy.save
+        dummy.update_column(:document_file_name, 'image.png')
+        dummy.update_column(:document_content_type, 'image/png')
+        dummy.update_column(:document_fingerprint, 'c5591c5ae4d01cae00d27b1cfb95fb2e')
+        dummy.destroy
+        expect(dummy.document_file_name).to eq nil
+        expect(dummy.document_content_type).to eq nil
+        expect(dummy.document_fingerprint).to eq nil
       end
     end
   end
@@ -53,9 +54,9 @@ describe 'Paperclip::Storage::GoogleDrive' do
               path: proc { |style| "#{style}_#{id}_#{document.original_filename}" }
             }
         )
-        @dummy = Dummy.new
-        @dummy.document = File.new('spec/fixtures/image.png', 'rb')
-        expect{ @dummy.save }.to raise_error(Paperclip::Storage::GoogleDrive::FileExists)
+        dummy = Dummy.new
+        dummy.document = File.new('spec/fixtures/image.png', 'rb')
+        expect{ dummy.save }.to raise_error(Paperclip::Storage::GoogleDrive::FileExists)
       end
     end
 
@@ -101,6 +102,18 @@ describe 'Paperclip::Storage::GoogleDrive' do
         }
       )
       expect{ Dummy.new.save }.to raise_error(ArgumentError, 'You must set the public_folder_id option')
+    end
+  end
+
+  context 'Manage PDFs' do
+    it 'should upload a pdf file' do
+      VCR.use_cassette('upload_pdf') do
+        dummy.document = File.new('spec/fixtures/document.pdf', 'rb')
+        expect(dummy.document).to_not be_blank
+        expect(dummy.document).to be_present
+        expect(dummy.save).to be true
+        expect(dummy.document.url).to be_present
+      end
     end
   end
 end
